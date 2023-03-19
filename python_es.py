d414ba348b873a442b73e1b61058836cf22518c6
Olga Knutova <G00425661@atu.ie>
2023-03-19 13:17:54 +0000

    week 7 task

---
+++ python_es.py
@@ -0,0 +1,27 @@
+# python_es.py
+# Author: Olga Knutova
+# Description: program reads in a text file and outputs the number of e's it contains
+
+import sys
+
+def count_letter (count, let='e'):
+    with open (FILENAME, 'r') as f:
+            for line in f:
+                    for char in line: 
+
+#https://theprogrammingexpert.com/python-read-file-character-by-character/?utm_content=cmp-true
+                        #read_data = f.read()
+                        if char == let :
+
+                            count= count+1
+                                
+    return count
+
+FILENAME = sys.argv[1]
+#https://askubuntu.com/questions/1059579/input-the-filename-in-the-commandline-as-an-argument-in-python
+count = 0
+let ='e' # Note: We will count only lowcase e's in this case. 
+
+ans= count_letter(count)
+print (ans)
+
