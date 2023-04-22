0b87e6f0005e036334c7b36c0ce550c52c648091
Olga Knutova <G00425661@atu.ie>
2023-04-22 20:03:02 +0100

    Es and missing argument

--- python_es.py
+++ python_es.py
@@ -7,6 +7,7 @@ import sys
 def count_letter (count, let='e'):
     with open (FILENAME, 'r') as f:
             for line in f:
+                    #line=line.lower()   Convert text to lower case if you want to count both lower and upper letter e together
                     for char in line: 
 
 #https://theprogrammingexpert.com/python-read-file-character-by-character/?utm_content=cmp-true
@@ -17,8 +18,14 @@ def count_letter (count, let='e'):
                                 
     return count
 
+if len(sys.argv)!=2:
+    exit ("Please enter a text file as an argument on the command line")
+    #https://stackoverflow.com/questions/2194163/python-empty-argument
+
 FILENAME = sys.argv[1]
+
 #https://askubuntu.com/questions/1059579/input-the-filename-in-the-commandline-as-an-argument-in-python
+
 count = 0
 let ='e' # Note: We will count only lowcase e's in this case. 
 
